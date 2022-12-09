6b8e15e967b1a8d5e2578c74067d9b364980f4cc
iftakher <26634250+iftakhermasum@users.noreply.github.com>
2022-12-09 16:33:19 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+jQuery(document).ready(function(){
 
+    //-----> owl carousel
+    jQuery('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: true,
+        autoplay:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+
+
+
+    
+    //-----> Menu hambarger
+    jQuery(".hambargerbox").click(function(){
+        jQuery(".mainmenuwrap").toggle();
+    });
+});
